f0da91f57b543ff067b9489deb6cf6d269fc4956
kkaniap <xkaniax@gmail.com>
2020-07-31 11:56:44 +0200

    Initial commit

---
+++ src/test/java/io/spring/guides/GuidesApplicationTests.java
@@ -0,0 +1,13 @@
+package io.spring.guides;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuidesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
