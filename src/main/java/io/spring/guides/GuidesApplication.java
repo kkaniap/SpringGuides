5d10dcc79201835a674048d563c2db58b9e4dac7
kkaniap <xkaniax@gmail.com>
2020-08-01 13:03:47 +0200

    Sample implementation of consuming a REST API

--- src/main/java/io/spring/guides/GuidesApplication.java
+++ src/main/java/io/spring/guides/GuidesApplication.java
@@ -1,13 +1,38 @@
 package io.spring.guides;
 
+import io.spring.guides.model.Quote;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @SpringBootApplication
 public class GuidesApplication {
 
+
+    private static final Logger log = Logger.getLogger(GuidesApplication.class.getName());
+
     public static void main(String[] args) {
         SpringApplication.run(GuidesApplication.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
+    }
+
+    @Bean
+    public CommandLineRunner run(RestTemplate restTemplate){
+        return args -> {
+            Quote quote = restTemplate.getForObject(
+                    "https://gturnquist-quoters.cfapps.io/api/random", Quote.class);
+            log.log(Level.INFO, quote.toString());
+        };
     }
 
 }
